a6bc149913ba323a3c2faef28e5aac3b156b56ac
maxinelobato <maxinelobato89@hotmail.com>
2024-07-09 17:49:33 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
 import { ButtonFloatWttp } from './components/Buttons'
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
+
+const GTM_ID = 'GTM-M9DBV4SH'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -23,16 +25,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-M9DBV4SH" />
+      <Script id="google-tag-manager" strategy="worker">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body className={`${roboto.className} bg-white antialiased`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M9DBV4SH"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
         {children}
         <ButtonFloatWttp />
       </body>
